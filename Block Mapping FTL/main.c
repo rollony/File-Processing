302c37b8ee590a4f22cf01514758d2af6a3ce05f
rollony <rollony215@gmail.com>
2021-06-23 20:25:07 +0900

    Update main.c

--- Block Mapping FTL/main.c
+++ Block Mapping FTL/main.c
@@ -1,7 +1,3 @@
-// 
-// 과제3의 채점 프로그램은 기본적으로 아래와 같이 동작함
-//
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -15,11 +11,6 @@ void ftl_open();
 void ftl_write(int lsn, char *sectorbuf);
 void ftl_read(int lsn, char *sectorbuf);
 
-//
-// 이 함수는 file system의 역할을 수행한다고 생각하면 되고,
-// file system이 flash memory로부터 512B씩 데이터를 저장하거나 데이터를 읽어 오기 위해서는
-// 각자 구현한 FTL의 ftl_write()와 ftl_read()를 호출하면 됨
-//
 int main(int argc, char *argv[])
 {
 	char *blockbuf;
@@ -33,9 +24,8 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	   
-    //
+    
     // flash memory의 모든 바이트를 '0xff'로 초기화한다.
-    // 
     blockbuf = (char *)malloc(BLOCK_SIZE);
 	memset(blockbuf, 0xFF, BLOCK_SIZE);
 
@@ -47,10 +37,6 @@ int main(int argc, char *argv[])
 	free(blockbuf);
 
 	ftl_open();    // ftl_read(), ftl_write() 호출하기 전에 이 함수를 반드시 호출해야 함
-
-	//
-	// ftl_write() 및 ftl_read() 테스트 코드 작성
-	//
 
 
 	fclose(flashfp);
